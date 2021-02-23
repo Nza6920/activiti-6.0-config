3bfd905f7eb19955649ea0cd6762dc6ac0fc6a93
NiuZian <zian.niu@gdcccn.com>
2021-02-23 13:04:58 +0800

    add springboot 整合 activiti

--- README.md
+++ README.md
@@ -8,5 +8,10 @@
 
 >> ## bpmn-2.0 (bpmn2.0流程组件的使用案例)
 
+>> ## springboot (集成springboot-2.3.4.RELEASE)
+>>> 1. 克隆项目 [6.0.0-boot2](https://github.com/Nza6920/Activiti/tree/6.0.0-boot2)
+>>> 2. 参见 README.md
+
+
 
 
--- springboot/src/test/java/com/niu/springboot/ApplicationTest.java
+++ springboot/src/test/java/com/niu/springboot/ApplicationTest.java
@@ -1,7 +1,12 @@
 package com.niu.springboot;
 
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,7 +16,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class ApplicationTest {
+
+    private static final Logger log = LoggerFactory.getLogger(ApplicationTest.class);
+
+    @Autowired
+    private RuntimeService runtimeService;
+
     @Test
-    public void contextLoads() {
+    public void testActiviti() {
+
+        log.info("测试 Spring 集成.......");
+
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("my_process");
+        log.info("启动流程: {}", processInstance.getProcessDefinitionKey());
     }
 }
